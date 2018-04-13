5aca079d78419ac996c6b60f74e3767dc2f0285d
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2018-04-13 16:53:03 +0200

    Added example helper functions for a different way of taking into account the safe states

--- src/optdet/scots_examples_helper.hh
+++ src/optdet/scots_examples_helper.hh
@@ -81,6 +81,166 @@ namespace tud {
                     }
 
                     /**
+                     * Allows to compute reach specification.
+                     *
+                     * @param cudd_manager the cudd manager
+                     * @param trans_rel the transition relation of the system
+                     * @param sym_model the symbolic model of the system
+                     * @param enforce_pred the enforceable predecessor based on the symbolic model
+                     * @param safe_states_bdd the safe states which one is allowed to go through
+                     * @param goal_states_bdd the goal states to reach and stay within
+                     * @param ctrl_bdd the reference to the output BDD for storing the controller
+                     */
+                    template<typename state_type, typename input_type>
+                    void reach(Cudd & cudd_manager, BDD trans_rel,
+                               const SymbolicModel<state_type, input_type> & sym_model,
+                               const SymbolicSet & states_set,
+                               const SymbolicSet & inputs_set,
+                               const BDD & safe_states_bdd,
+                               const BDD & goal_states_bdd,
+                               BDD & ctrl_bdd) {
+                        //Take into account the safe states, since we use the
+                        //fixed point algorithm based on backwards reachability
+                        //this is enough to ensure the Globally<sage_states_bdd>
+                        trans_rel &= safe_states_bdd;
+                      
+                        //Set up enf_pre computation
+                        EnfPre enforce_pred(cudd_manager, trans_rel, sym_model);
+                      
+                        /*The actual goal set, excludes the unsafe ones*/
+                        BDD G = goal_states_bdd & safe_states_bdd;
+                      
+                        /*The outer iteration variables*/
+                        BDD X = cudd_manager.bddOne();
+                        BDD XX = cudd_manager.bddZero();
+                      
+                        /*The BDD cube for inputs filtering*/
+                        BDD U = inputs_set.get_cube(cudd_manager);
+                      
+                        /*Initialize the controller*/
+                        ctrl_bdd = cudd_manager.bddZero();
+                      
+                        size_t cnt = 1;
+                        while (XX != X) {
+                            X = XX;
+                            XX = enforce_pred(X) | G;
+                            BDD N = XX & (!(ctrl_bdd.ExistAbstract(U)));
+                            ctrl_bdd = ctrl_bdd | N;
+                            print_progress(cnt++);
+                        }
+                      
+                        cout << endl << "Winning domain size: "
+                        << states_set.get_size(cudd_manager, ctrl_bdd)
+                        << endl;
+
+                    }
+                  
+                    /**
+                     * Allows to compute reach specification.
+                     * 
+                     * @param cudd_manager the cudd manager
+                     * @param enforce_pred the enforceable predecessor based on the symbolic model
+                     * @param safe_states_bdd the safe states which one is allowed to go through
+                     * @param goal_states_bdd the goal states to reach and stay within
+                     * @param ctrl_bdd the reference to the output BDD for storing the controller
+                     */
+                    void reach(Cudd & cudd_manager,
+                            const EnfPre & enforce_pred,
+                            const SymbolicSet & states_set,
+                            const SymbolicSet & inputs_set,
+                            const BDD & safe_states_bdd,
+                            const BDD & goal_states_bdd,
+                            BDD & ctrl_bdd) {
+                        /*The actual goal set, excludes the unsafe ones*/
+                        BDD G = goal_states_bdd & safe_states_bdd;
+
+                        /*The outer iteration variables*/
+                        BDD X = cudd_manager.bddOne();
+                        BDD XX = cudd_manager.bddZero();
+
+                        /*The BDD cube for inputs filtering*/
+                        BDD U = inputs_set.get_cube(cudd_manager);
+
+                        /*Initialize the controller*/
+                        ctrl_bdd = cudd_manager.bddZero();
+
+                        size_t cnt = 1;
+                        while (XX != X) {
+                            X = XX;
+                            XX = (safe_states_bdd & enforce_pred(X)) | G;
+                            BDD N = XX & (!(ctrl_bdd.ExistAbstract(U)));
+                            ctrl_bdd = ctrl_bdd | N;
+                            print_progress(cnt++);
+                        }
+
+                        cout << endl << "Winning domain size: "
+                                << states_set.get_size(cudd_manager, ctrl_bdd)
+                                << endl;
+                    }
+
+
+                    /**
+                     * Allows to compute reach specification.
+                     *
+                     * @param cudd_manager the cudd manager
+                     * @param trans_rel the transition relation of the system
+                     * @param sym_model the symbolic model of the system
+                     * @param enforce_pred the enforceable predecessor based on the symbolic model
+                     * @param safe_states_bdd the safe states which one is allowed to go through
+                     * @param goal_states_bdd the goal states to reach and stay within
+                     * @param ctrl_bdd the reference to the output BDD for storing the controller
+                     */
+                    template<typename state_type, typename input_type>
+                    void reach_and_stay(Cudd & cudd_manager, BDD trans_rel,
+                                        const SymbolicModel<state_type, input_type> & sym_model,
+                                        const SymbolicSet & states_set,
+                                        const SymbolicSet & inputs_set,
+                                        const BDD & safe_states_bdd,
+                                        const BDD & goal_states_bdd,
+                                        BDD & ctrl_bdd) {
+                        //Take into account the safe states, since we use the
+                        //fixed point algorithm based on backwards reachability
+                        //this is enough to ensure the Globally<sage_states_bdd>
+                        trans_rel &= safe_states_bdd;
+                    
+                        //Set up enf_pre computation
+                        EnfPre enforce_pred(cudd_manager, trans_rel, sym_model);
+                      
+                        /*The actual goal set, excludes the unsafe ones*/
+                        BDD G = goal_states_bdd & safe_states_bdd;
+                    
+                        /*The outer iteration variables*/
+                        BDD Y = cudd_manager.bddOne();
+                        BDD YY = cudd_manager.bddZero();
+                    
+                        /*The BDD cube for inputs filtering*/
+                        BDD U = inputs_set.get_cube(cudd_manager);
+                    
+                        /*Initialize the controller*/
+                        ctrl_bdd = cudd_manager.bddZero();
+                    
+                        size_t cnt = 1;
+                        while (YY != Y) {
+                            Y = YY;
+                            BDD Z = enforce_pred(Y);
+                            BDD X = cudd_manager.bddZero();
+                            BDD XX = G | Z;
+                            while (XX != X) {
+                                X = XX;
+                                XX = (X & enforce_pred(X)) | Z;
+                            }
+                            YY = X;
+                            BDD N = YY & (!(ctrl_bdd.ExistAbstract(U)));
+                            ctrl_bdd = ctrl_bdd | N;
+                            print_progress(cnt++);
+                        }
+                      
+                        cout << endl << "Winning domain size: "
+                        << states_set.get_size(cudd_manager, ctrl_bdd)
+                        << endl;
+                    }
+                  
+                    /**
                      * Allows to compute reach and stay specification.
                      * 
                      * @param cudd_manager the cudd manager
@@ -96,19 +256,20 @@ namespace tud {
                             const BDD & safe_states_bdd,
                             const BDD & goal_states_bdd,
                             BDD & ctrl_bdd) {
-                        /*The actual dagoal set, excludes the unsafe ones*/
+                        /*The actual goal set, excludes the unsafe ones*/
                         BDD G = goal_states_bdd & safe_states_bdd;
 
                         /*The outer iteration variables*/
                         BDD Y = safe_states_bdd;
                         BDD YY = cudd_manager.bddZero();
-
+                        
                         /*The BDD cube for inputs filtering*/
                         BDD U = inputs_set.get_cube(cudd_manager);
-
+                      
                         /*Initialize the controller*/
                         ctrl_bdd = cudd_manager.bddZero();
-
+                      
+                        size_t cnt = 1;
                         while (YY != Y) {
                             Y = YY;
                             BDD Z = safe_states_bdd & enforce_pred(Y);
@@ -116,20 +277,21 @@ namespace tud {
                             BDD XX = G | Z;
                             while (XX != X) {
                                 X = XX;
-                                XX = Z | (X & enforce_pred(X));
+                                XX = (X & enforce_pred(X)) | Z;
                             }
                             YY = X;
                             BDD N = YY & (!(ctrl_bdd.ExistAbstract(U)));
                             ctrl_bdd = ctrl_bdd | N;
+                            print_progress(cnt++);
                         }
-
-                        cout << "Winning domain size: "
+                      
+                        cout << endl << "Winning domain size: "
                                 << states_set.get_size(cudd_manager, ctrl_bdd)
                                 << endl;
                     }
 
                     /**
-                     * Allows to create the controller's sybolic set, check that 
+                     * Allows to create the controller's symbolic set, check that 
                      * the controller does not contain unsafe states and store
                      * the controller into file.
                      * @param cudd_manager the cudd manager
